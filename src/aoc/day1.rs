6f991d80623b3c289debd84b22bd0650ce2ff40f
Dominik Liebler <liebler.dominik@gmail.com>
2022-12-01 11:30:17 +0100

    refactored day 1 using itertools

--- src/aoc/day1.rs
+++ src/aoc/day1.rs
@@ -1,37 +1,24 @@
-fn parse_elf(input: String) -> Vec<u32> {
-    return input.trim().split("\n").map(|x| x.parse::<u32>().unwrap()).collect();
-}
+use itertools::Itertools;
 
-fn input_to_vec(input: String) -> Vec<Vec<u32>> {
-    let by_elf: Vec<&str> = input.split("\n\n").collect();
-    let x = by_elf.iter().map(|x| parse_elf(x.to_string())).collect();
-
-    return x
-}
-
-fn sum_by_elf(input: Vec<Vec<u32>>) -> Vec<u32> {
-    return input.into_iter().map(|e| e.into_iter().reduce(|a, b| a + b).unwrap()).collect();
+fn top3_calories(input: String) -> Vec<u32> {
+    return input.split("\n\n")
+        .map(|x| x.lines().map(|l| l.parse::<u32>().unwrap()).sum())
+        .sorted()
+        .rev()
+        .take(3)
+        .collect()
 }
 
 #[allow(dead_code)]
 fn day1(input: String) -> u32 {
-    let numbers = input_to_vec(input);
-    let sums = sum_by_elf(numbers);
-
-    return *sums.iter().max().unwrap();
+    return top3_calories(input)[0];
 }
 
 #[allow(dead_code)]
 fn day1_part2(input: String) -> u32 {
-    let numbers = input_to_vec(input);
-    let mut sums = sum_by_elf(numbers);
-
-    sums.sort();
-    sums.reverse();
-
-    let top3 = &sums[0..3];
-
-    return top3.to_vec().into_iter().reduce(|a,b| a+ b).unwrap();
+    return top3_calories(input)[0..3]
+        .iter()
+        .sum();
 }
 
 #[cfg(test)]
