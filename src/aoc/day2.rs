ca6f05aaf296b87f5d42fdf4312b7a9d48bd4966
Dominik Liebler <liebler.dominik@gmail.com>
2022-12-02 08:20:31 +0100

    day 2 part 1

--- src/aoc/day2.rs
+++ src/aoc/day2.rs
@@ -1,24 +1,40 @@
-use itertools::Itertools;
+use std::collections::HashMap;
 
-fn top3_calories(input: String) -> Vec<u32> {
-    return input.split("\n\n")
-        .map(|x| x.lines().map(|l| l.parse::<u32>().unwrap()).sum())
-        .sorted()
-        .rev()
-        .take(3)
-        .collect()
+fn play(shapes: Vec<&str>) -> i32 {
+    let signs = HashMap::from([
+        ("A", 1), // rock
+        ("B", 2), // paper
+        ("C", 3), // scissors
+        ("X", 1), // rock
+        ("Y", 2), // paper
+        ("Z", 3), // scissors
+    ]);
+
+    let opponent = signs.get(&*shapes[0]).expect("Not a valid symbol");
+    let me = signs.get(&*shapes[1]).expect("Not a valid symbol");
+
+    let points = if me == opponent {
+        me + 3
+    } else if shapes == vec!["C", "X"] || shapes == vec!["A", "Y"] || shapes == vec!["B", "Z"] {
+        me + 6
+    } else {
+        me + 0
+    };
+
+    return points;
 }
 
 #[allow(dead_code)]
-fn day1(input: String) -> u32 {
-    return top3_calories(input)[0];
-}
-
-#[allow(dead_code)]
-fn day1_part2(input: String) -> u32 {
-    return top3_calories(input)[0..3]
-        .iter()
+fn day2(input: String) -> i32 {
+    return input
+        .lines()
+        .map(|l| play(l.split(" ").collect()))
         .sum();
+}
+
+#[allow(dead_code)]
+fn day2_part2(_input: String) -> i32 {
+    return 1;
 }
 
 #[cfg(test)]
@@ -27,17 +43,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_day1() -> Result<(), std::io::Error> {
-        assert_eq!(day1(fs::read_to_string("input/day1.txt")?), 24000);
-        //println!("{:?}", day1(fs::read_to_string("input/day1_full.txt")?));
-
+    fn test_day2() -> Result<(), std::io::Error> {
+        assert_eq!(day2(fs::read_to_string("input/day2.txt")?), 15);
+        //println!("day2 part 1: {:?}", day2(fs::read_to_string("input/day2_full.txt")?));
         Ok(())
     }
 
     #[test]
-    fn test_day1_part2() -> Result<(), std::io::Error> {
-        assert_eq!(day1_part2(fs::read_to_string("input/day1.txt")?), 45000);
-        //println!("{:?}", day1_part2(fs::read_to_string("input/day1_full.txt")?));
+    fn test_day2_part2() -> Result<(), std::io::Error> {
+        assert_eq!(day2_part2(fs::read_to_string("input/day2.txt")?), 12);
+        println!("day 2 part 2: {:?}", day2_part2(fs::read_to_string("input/day2_full.txt")?));
         Ok(())
     }
 }
