2617e2df50291f349cd449a33dac9122d6a91ff1
Dominik Liebler <liebler.dominik@gmail.com>
2022-12-02 08:47:42 +0100

    day 2 part 2 + refactoring

--- src/aoc/day2.rs
+++ src/aoc/day2.rs
@@ -1,40 +1,47 @@
-use std::collections::HashMap;
-
-fn play(shapes: Vec<&str>) -> i32 {
-    let signs = HashMap::from([
-        ("A", 1), // rock
-        ("B", 2), // paper
-        ("C", 3), // scissors
-        ("X", 1), // rock
-        ("Y", 2), // paper
-        ("Z", 3), // scissors
-    ]);
-
-    let opponent = signs.get(&*shapes[0]).expect("Not a valid symbol");
-    let me = signs.get(&*shapes[1]).expect("Not a valid symbol");
-
-    let points = if me == opponent {
-        me + 3
-    } else if shapes == vec!["C", "X"] || shapes == vec!["A", "Y"] || shapes == vec!["B", "Z"] {
-        me + 6
-    } else {
-        me + 0
+fn play(shapes: &str) -> i32 {
+    return match shapes {
+        "A X" => 4,
+        "A Y" => 8,
+        "A Z" => 3,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 7,
+        "C Y" => 2,
+        "C Z" => 6,
+        _ => 0
     };
+}
 
-    return points;
+fn play_part2(shapes: &str) -> i32 {
+     return match shapes {
+        "A X" => 3,
+        "A Y" => 4,
+        "A Z" => 8,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 2,
+        "C Y" => 6,
+        "C Z" => 7,
+         _ => 0
+     };
 }
 
 #[allow(dead_code)]
 fn day2(input: String) -> i32 {
     return input
         .lines()
-        .map(|l| play(l.split(" ").collect()))
+        .map(|l| play(l))
         .sum();
 }
 
 #[allow(dead_code)]
-fn day2_part2(_input: String) -> i32 {
-    return 1;
+fn day2_part2(input: String) -> i32 {
+    return input
+        .lines()
+        .map(|l| play_part2(l))
+        .sum();
 }
 
 #[cfg(test)]
@@ -52,7 +59,7 @@ mod tests {
     #[test]
     fn test_day2_part2() -> Result<(), std::io::Error> {
         assert_eq!(day2_part2(fs::read_to_string("input/day2.txt")?), 12);
-        println!("day 2 part 2: {:?}", day2_part2(fs::read_to_string("input/day2_full.txt")?));
+        //println!("day 2 part 2: {:?}", day2_part2(fs::read_to_string("input/day2_full.txt")?));
         Ok(())
     }
 }
